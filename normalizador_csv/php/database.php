969b87d11383e78d48f22f7975e46e1c2c3f5c24
Enzo Ryan Simette <enzoryansimetteplr@gmail.com>
2026-01-28 11:33:21 -0300

    conexão com banco de dados

---
+++ normalizador_csv/php/database.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace sys4soft;
+
+use PDO;
+use PDOException;
+use stdClass;
+
+//* ========================
+//* = CONECTION WITH MYSQL =
+//* ========================
+
+class Database
+{
+    // PROPERTIES
+    private $_host;
+    private $_database;
+    private $_username;
+    private $_password;
+    private $_charset;
+    private $return_type;
+
+    public function __construct($cfg_options, $return_type = 'object')
+    {
+        // SET CONECTION CONFIGURATIONS
+        $this->_host = $cfg_options['host'];
+        $this->_database = $cfg_options['database'];
+        $this->_username = $cfg_options['username'];
+        $this->_password = $cfg_options['password'];
+
+        // SET THE RETURN TYPE
+        if (!empty($return_type) && $return_type == 'object') { // VERIFY IF THE RETURN IS A OBJECT
+            $this->return_type = PDO::FETCH_OBJ; // RETURN OBJECT
+        } else {
+            $this->return_type = PDO::FETCH_ASSOC; // RETURN ASSOCIATIVE ARRAY
+        }
+    }
+
+    private function _result($status, $message, $sql, $results, $affected_rows, $last_id)
+    {
+        $tmp = new stdClass();
+        $tmp->status = $status;
+        $tmp->message = $message;
+        $tmp->query = $sql;
+        $tmp->results = $results;
+        $tmp->affected_rows = $affected_rows;
+        $tmp->last_id = $last_id;
+        return $tmp;
+    }
+
+    public function execute_non_query($sql, $parameters = null)
+    {
+
+        // EXECUTES A QUERY WITHOUT RESULTS
+        // CONECTION
+        $conection = new PDO(
+            'mysql:host=' . $this->_host . ';dbname=' . $this->_database . ';charset=' . $this->_charset,
+            $this->_username,
+            $this->_password,
+            array(PDO::ATTR_PERSISTENT => true)
+        );
+
+        // INIT TRANSACTION
+        $conection->beginTransaction();
+
+        // PREPARE AND EXECUTE THE QUERY
+        try {
+            $db = $conection->prepare($sql);
+            if (!empty($parameters)) {
+                $db->execute($parameters);
+            } else {
+                $db->execute();
+            }
+
+            // LAST INSERTED ID
+            $last_inserted_id = $conection->lastInsertId();
+
+            // FINISH TRANSACTION
+            $conection->commit();
+        } catch (PDOException $ex) {
+            // UNDO ALL SQL OPERATIONS ON ERROR
+            $conection->rollBack();
+            $conection = null;
+            return $this->_result('Error:', $ex->getMessage(), $sql, null, 0, null);
+        }
+        // CLOSE CONNECTION
+        $conection = null;
+        return $this->_result('SUCESS', 'sucess', $sql, null, $db->rowCount(), $last_inserted_id);
+    }
+
+    public function execute_query($sql, $parameters = null)
+    {
+        // EXECUTE A QUERY WITH RESULTS
+        // CONECTION
+        $conection = new PDO(
+            'mysql:host=' . $this->_host . ';dbname=' . $this->_database . ';charset=' . $this->_charset,
+            $this->_username,
+            $this->_password,
+            array(PDO::ATTR_PERSISTENT => true)
+        );
+
+        $results = null;
+
+        try {
+            $db = $conection->prepare($sql);
+            if (!empty($parameters)) {
+                $db->execute($parameters);
+            } else {
+                $db->execute();
+            }
+            $results = $db->fetchAll($this->return_type);
+        } catch (PDOException $ex) {
+            $conection = null;
+            return $this->_result('Error:', $ex->getMessage(), $sql, null, 0, null);
+        }
+
+        // CLOSE CONNECTION
+        $conection = null;
+        return $this->_result('SUCESS', 'sucess', $sql, $results, $db->rowCount(), null);
+    }
+}
--- normalizador_csv/php/normalizador.php
+++ normalizador_csv/php/normalizador.php
@@ -1,1 +1,14 @@
 <?php
+$caminho = $_FILES["arquivo"]["tmp_name"];
+$caminho = str_replace('\\', '/', $caminho);
+
+//? $$$$$$$$$$$$$$
+//? $ PROTECTION $
+//? $$$$$$$$$$$$$$
+
+// $usr_psswd = [
+//     ':usr' => $_POST['text_username'],
+//     ':psswd' => $_POST['text_password']
+// ];
+
+// $comando = $ligacao->prepare("SELECT * FROM usuarios WHERE username = :usr AND password = :psswd");
